9dd7b7fabe7e43b835e30c8cae0090aa9169a718
Patryk <patryk-41@wp.pl>
2018-06-06 18:56:19 +0200

    Add animation on X coordinate, new picture

--- Project5/MyForm.h
+++ Project5/MyForm.h
@@ -36,8 +36,11 @@ namespace Project5 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::Button^  button3;
 	protected:
 
 	protected:
@@ -58,56 +61,93 @@ namespace Project5 {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 446);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"++";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(41, -1);
+			this->pictureBox1->Location = System::Drawing::Point(12, 12);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(663, 362);
-			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->Size = System::Drawing::Size(658, 428);
+			this->pictureBox1->TabIndex = 3;
 			this->pictureBox1->TabStop = false;
-			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click_1);
 			// 
-			// button1
+			// pictureBox2
 			// 
-			this->button1->Location = System::Drawing::Point(711, 363);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 2;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(326, 110);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(36, 18);
+			this->pictureBox2->TabIndex = 4;
+			this->pictureBox2->TabStop = false;
+			this->pictureBox2->Click += gcnew System::EventHandler(this, &MyForm::pictureBox2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(93, 446);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"--";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1278, 574);
-			this->Controls->Add(this->button1);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->pictureBox2);
 			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"Project";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+public: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
 }
+	int x = 327, y = 110;
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100; i++) {
 			
-			//Sleep(50);
-			this->pictureBox1->Location = System::Drawing::Point(41+ i*0.5, -1);
+			Sleep(5);
+			this->pictureBox2->Location = System::Drawing::Point(x++,y);
 		}
 	}
-	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	
+	private: System::Void pictureBox1_Click_1(System::Object^  sender, System::EventArgs^  e) {
 	}
-	};
+	private: System::Void pictureBox2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+	for (int i = 0; i < 100; i++) {
+
+		Sleep(5);
+		this->pictureBox2->Location = System::Drawing::Point(x--, y);
+	}
+}
+};
 };
